c919ab07ddd8c5f287d44e881a56f9a892f2490f
seeren <consultant@seeren.fr>
2020-12-13 15:49:26 +0100

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,64 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+
+module.exports = {
+    entry: [
+        './src/app.js',
+        './src/assets/scss/app.scss',
+    ],
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'app.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.html$/,
+                exclude: /node_modules/,
+                use: 'raw-loader',
+            },
+            {
+                test: /\.scss$/,
+                exclude: /node_modules/,
+                use: [
+                    MiniCssExtractPlugin.loader,
+                    'css-loader',
+                    'sass-loader',
+                ],
+            },
+            {
+                test: /\.(jpg|png|woff|woff2|eot|ttf|svg)$/,
+                loader: 'file-loader',
+                options: {
+                    name: '[path][name].[ext]',
+                },
+            },
+        ],
+    },
+    watchOptions: {
+        ignored: [
+            '/node_modules/',
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({ 
+            filename: 'index.css' 
+        }),
+        new BrowserSyncPlugin({
+            host: 'localhost',
+            port: 3000,
+            files: ['index.html'],
+            server: {
+                baseDir: './',
+                middleware: (req, res, next) => (-1 === req.url.indexOf('.') && '/' !== req.url
+                    ? res.end(res.writeHead(302, { Location: '/' }))
+                    : next()),
+            },
+        }),
+    ],
+};
